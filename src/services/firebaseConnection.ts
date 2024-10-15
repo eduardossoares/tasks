88e16580e4737766a6de169a8b0d5968f1f7afea
Eduardo Soares <128731192+eduardossoares@users.noreply.github.com>
2024-10-15 16:13:58 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQFPLQeuq9tjYu-zDaTcCT2FXx8qszoV4",
-    authDomain: "tasks-c6940.firebaseapp.com",
-    projectId: "tasks-c6940",
-    storageBucket: "tasks-c6940.appspot.com",
-    messagingSenderId: "379113516652",
-    appId: "1:379113516652:web:002da0b34d65ccb03fb249",
-    measurementId: "G-1YP99J5KRY"
+    apiKey: VITE_FIREBASE_API_KEY,
+    authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: VITE_FIREBASE_PROJECT_ID,
+    storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
